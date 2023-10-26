2db2c01a9e32769a33c4970821486b59383fc730
meesp <jennifermeesp@gmail.com>
2023-10-25 21:27:00 -0300

    Implementacion de Layout, clase mensaje, Mensaje y clase dBAdapater.

---
+++ app/src/main/java/com/jennifer/sqliteoperaciones/mensaje.java
@@ -0,0 +1,10 @@
+package com.jennifer.sqliteoperaciones;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class mensaje {
+    public static void aviso(Context context, String aviso){
+        Toast.makeText(context, aviso, Toast.LENGTH_LONG).show();
+    }
+}
